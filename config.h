d44fe213587933de10745e23d749595958b03b43
Philip Olesen <nb.philip@gmail.com>
2021-05-11 11:41:46 +0200

    laptop whatever

--- config.h
+++ config.h
@@ -73,6 +73,10 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_red, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *raisevol[] = { "amixer", "sset", "Master", "2%+" };
+static const char *lowervol[] = { "amixer", "sset", "Master", "2%-" };
+static const char *mute[]     = { "amixer", "sset", "Master", "toggle" };
+static const char *playpause[] = { "mpc", "toggle" };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
