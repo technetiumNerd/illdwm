6a9ef7bd728694bcddf03f4b5aba504362dd094f
Philip Olesen <nb.philip@gmail.com>
2021-05-28 11:37:32 +0200

    config ...

--- config.def.h
+++ config.def.h
@@ -13,6 +13,7 @@ static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
+static const char col_cyan[]        = "#005577";
 
 static const char col_orange[]      = "#704000";
 static const char col_red[]         = "#550000";
