fa120fb33ef81ba44dc3c739d639ba28c1c8c1f7
Philip Olesen <nb.philip@gmail.com>
2021-12-11 07:28:43 +0100

    font, winsizedefault changes

--- config.def.h
+++ config.def.h
@@ -6,8 +6,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "iosevka term:size=12" };
-static const char dmenufont[]       = "iosevka term:size=12";
+static const char *fonts[]          = { "iosevka term:size=11" };
+static const char dmenufont[]       = "iosevka term:size=11";
 static const char col_gray0[]       = "#080808";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -42,11 +42,11 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	//{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.6; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
